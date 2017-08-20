87e0b4b999138a9b41a71b4d9bbe689eec44aa5e
Patsy Price <patsy.m.price@gmail.com>
2017-08-19 22:44:48 -0700

    create employee endpoint

--- app/api/employee/data.rb
+++ app/api/employee/data.rb
@@ -6,6 +6,25 @@ module Employee
       get do
         EmpData.all
       end
+
+      desc "Create new Employee"
+      # paramater validation
+      params do
+        requires :name, type: String
+        requires :address, type: String
+        requires :age, type: Integer
+      end
+      # post a request to create employee
+      post do
+        EmpData.create!({
+            name: params[:name],
+            address: params[:address],
+            age: params[:age]
+         })
+      end
+
     end
+
   end
+
 end
